5ecc257e032f03381d18f95e72111dadfa690fbd
Andreas Signer <asigner@gmail.com>
2013-09-03 21:18:30 +0000

    better logging
    git-svn-id: https://svn.nische4.com/kosmos-cp1/trunk@3909 f7503561-2f13-4643-84d6-ae4cf32e837a

--- src/com/asigner/cp1/emulation/DataPort.java
+++ src/com/asigner/cp1/emulation/DataPort.java
@@ -2,8 +2,11 @@ package com.asigner.cp1.emulation;
 
 import java.util.LinkedList;
 import java.util.List;
+import java.util.logging.Logger;
 
 public class DataPort {
+
+    private static final Logger logger = Logger.getLogger(DataPort.class.getName());
 
     private final String name;
     private int value;
@@ -16,10 +19,17 @@ public class DataPort {
     }
 
     public int read() {
-        return value & 0xff;
+        int res = value & 0xff;
+        String binVal = String.format("%8s", Integer.toBinaryString(res)).replace(' ', '0');
+        String message = String.format("Port %3s: reading 0x%02x (0b%s)", name, res, binVal);
+        logger.info(message);
+        return res;
     }
 
     public void write(int value) {
+        String binVal = String.format("%8s", Integer.toBinaryString(value)).replace(' ', '0');
+        String message = String.format("Port %3s: writing 0x%02x (0b%s)", name, value, binVal);
+        logger.info(message);
         if (value != this.value) {
             int oldValue = this.value;
             this.value = value;
