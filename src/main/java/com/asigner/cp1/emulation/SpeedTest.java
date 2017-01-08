96feabc7d7479b54176e9c73b563c5a94fa64dad
Andreas Signer <asigner@gmail.com>
2017-01-08 22:52:59 +0100

    add small program to measure whether the emultor is fast enough

---
+++ src/main/java/com/asigner/cp1/emulation/SpeedTest.java
@@ -0,0 +1,47 @@
+// Copyright 2017 Andreas Signer. All rights reserved.
+
+package com.asigner.cp1.emulation;
+
+import com.asigner.cp1.emulation.ui.CpuUI;
+
+public class SpeedTest {
+
+    /**
+     * Launch the application.
+     * @param args
+     */
+    public static void main(String[] args) {
+        try {
+            Rom rom = new Rom(Main.class.getResourceAsStream("/com/asigner/cp1/CP1.bin"));
+            Ram ram8049 = new Ram(128);
+            Ram ram8155 = new Ram(256);
+            DataPort bus = new DataPort("BUS");
+            DataPort p1 = new DataPort("P1");
+            DataPort p2 = new DataPort("P2");
+            Intel8049 cpu = new Intel8049(ram8049, rom, bus, p1, p2);
+            Intel8155 pid = new Intel8155(bus, ram8155);
+
+            // Connect the relevant pins
+            cpu.pinALE.connectTo(pid.pinALE);
+            cpu.pinRDLowActive.connectTo(pid.pinRDLowActive);
+            cpu.pinWRLowActive.connectTo(pid.pinWRLowActive);
+            p2.connectBitTo(6, pid.pinReset);
+            p2.connectBitTo(5, pid.pinIO);
+            p2.connectBitTo(4, pid.pinCELowActive);
+
+            for (;;) {
+                int cycles = 1_000_000;
+                long start = System.nanoTime();
+                cpu.execute(cycles);
+                long end = System.nanoTime();
+                long dt = end - start;
+                double perCycle = ((double) dt) / cycles;
+                System.err.println("Executing " + cycles + " cycles took " + dt + " ns, " + perCycle + " ns per cycle" + (perCycle > 2_500_000 ? ", TOO SLOW!" : ""));
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+}
