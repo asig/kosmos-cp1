88a5a35cd3b7cef8bf94f4f5f2e945366bffeca6
Andreas Signer <asigner@gmail.com>
2017-02-03 01:30:46 +0100

    fixes

--- src/main/resources/com/asigner/cp1/listings/listing19.asm
+++ src/main/resources/com/asigner/cp1/listings/listing19.asm
@@ -1,6 +1,6 @@
 ; Listing 19: Automatisch speichernder Lottozahlen-Generator
 
-        .ORG 11
+        .ORG 1
 
         AKO 001     ; \
         ABS 100     ; |
@@ -25,7 +25,7 @@ add     ADD 100     ; Addiere ,,1 " zum Akku-Inhalt
         ABS 104     ; Speichere den neuen Akku-Inhalt in 104
 check   P1E 001     ; Bringe die Information von Klemme 1 des Port 1 in den Akku
         VGL 103     ; Prüfe, ob Taste 1 gedrückt ist
-        SPB checkkey ; Wenn Taste gedrückt, springe zur Abfrage zurück
+        SPB check   ; Wenn Taste gedrückt, springe zur Abfrage zurück
         SPU start   ; Sonst springe zurück zu einem neuen Durchlauf
 ng      VGL 101     ; Prüfe, ob die Zählgrenze erreicht ist
         SPB start   ; Wenn ja, springe zum Anfang zurück
