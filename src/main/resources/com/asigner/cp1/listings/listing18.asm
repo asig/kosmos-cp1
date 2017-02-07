e170a1d2be6b29db678ddcc67c81ac8b750e3873
Andreas Signer <asigner@gmail.com>
2017-02-07 11:24:16 +0100

    cleanup

--- src/main/resources/com/asigner/cp1/listings/listing18.asm
+++ src/main/resources/com/asigner/cp1/listings/listing18.asm
@@ -1,6 +1,7 @@
 ; Listing 18: Blinkprogramm mit zwei Lämpchen im Parallelbetrieb
 
         .ORG 1
+
 start   AKO 000 ; In den Akku eine „0" laden
         P1A 000 ; Akku-Inhalt als Dualzahl auf Port 1 bringen
         VZG 250 ; 250 ms verzögern
