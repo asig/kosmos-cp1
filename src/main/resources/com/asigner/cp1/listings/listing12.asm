3cf6e715e2862d3c7556d7b74e125b41625c78b5
Andreas Signer <asigner@gmail.com>
2017-02-07 19:12:13 +0100

    cleanup

--- src/main/resources/com/asigner/cp1/listings/listing12.asm
+++ src/main/resources/com/asigner/cp1/listings/listing12.asm
@@ -2,7 +2,7 @@
 
         .ORG 1
 
-start   AKO 000     ; In den Akku eine „0 " laden
+start   AKO 000     ; In den Akku eine „0" laden
         P1A 001     ; Akku-Inhalt auf Klemme 1 des Port 1 ausgeben
         P1A 002     ; Akku-Inhalt auf Klemme 2 des Port 1 ausgeben
 wait1   P1E 008     ; Information von Klemme 8 des Port 1 in den Akku bringen
@@ -13,14 +13,14 @@ l1      P1E 008     ; Information v. Klemme 8 des Port 1 in den Akku bringen
         VGL 101     ; Akku-Inhalt mit Inhalt von Zelle 101 vergleichen
         SPB l2      ; Wenn Gleichheit, dann auf 012 springen
         SPU l1      ; Wenn nicht Gleichheit, auf 008 springen
-l2      AKO 001     ; In den Akku eine „1 " laden
+l2      AKO 001     ; In den Akku eine „1" laden
         P1A 001     ; Akku-Inhalt auf Klemme 1 des Port 1 ausgeben
-        AKO 000     ; In den Akku eine „0 " laden
+        AKO 000     ; In den Akku eine „0" laden
         P1A 002     ; Akku-Inhalt auf Klemme 2 des Port 1 ausgeben
         VZG 250     ; 250 ms verzögern
-        AKO 000     ; In den Akku eine „0 " laden
+        AKO 000     ; In den Akku eine „0" laden
         P1A 001     ; Akku-Inhalt auf Klemme 1 des Port 1 ausgeben
-        AKO 001     ; In den Akku eine „1 " laden
+        AKO 001     ; In den Akku eine „1" laden
         P1A 002     ; Akku-Inhalt auf Klemme 2 des Port 1 ausgeben
         VZG 250     ; 250 ms verzögern
         P1E 008     ; Information v. Klemme 8 des Port 1 in den Akku bringen
