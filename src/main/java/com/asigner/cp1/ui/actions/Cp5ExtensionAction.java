475bc6e19c344f97fa7fffb52cef844c317dbae7
Andreas Signer <asigner@gmail.com>
2017-02-21 23:21:13 +0100

    actions for extensions

---
+++ src/main/java/com/asigner/cp1/ui/actions/Cp5ExtensionAction.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright (c) 2017 Andreas Signer <asigner@gmail.com>
+ *
+ * This file is part of kosmos-cp1.
+ *
+ * kosmos-cp1 is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * kosmos-cp1 is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with kosmos-cp1.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.asigner.cp1.ui.actions;
+
+import org.eclipse.jface.action.Action;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.MessageBox;
+
+public class Cp5ExtensionAction extends Action {
+
+    public Cp5ExtensionAction() {
+        super("CP5: Ein/Ausgabe-Universalinterface");
+        setChecked(true);
+    }
+
+    @Override
+    public void run() {
+        MessageBox mbox = new MessageBox(Display.getDefault().getActiveShell(), SWT.ICON_INFORMATION);
+        mbox.setMessage("Not implemented yet");
+        mbox.open();
+    }
+
+}
