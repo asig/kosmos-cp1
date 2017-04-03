638003b58046120950747f8d9351ab7f2736db69
Andreas Signer <asigner@gmail.com>
2017-04-03 12:08:40 +0200

    make the tests pass again

--- src/test/java/com/asigner/cp1/emulation/DataPortTest.java
+++ src/test/java/com/asigner/cp1/emulation/DataPortTest.java
@@ -21,12 +21,9 @@ package com.asigner.cp1.emulation;
 
 import org.junit.Test;
 
-import java.util.function.BiConsumer;
-
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
-import static sun.java2d.cmm.ColorTransform.In;
 
 public class DataPortTest {
 
@@ -43,6 +40,11 @@ public class DataPortTest {
         void write(int value) {
             this.value = value;
             this.writeCalled = true;
+        }
+
+        void reset() {
+            this.value = 0;
+            this.writeCalled = false;
         }
     }
 
@@ -151,33 +153,22 @@ public class DataPortTest {
     @Test
     public void testWritePartial() throws Exception {
         DataPort port = new DataPort("test");
-
-        RecordingInputPin p0 = new RecordingInputPin();
-        RecordingInputPin p1 = new RecordingInputPin();
-        RecordingInputPin p2 = new RecordingInputPin();
-        RecordingInputPin p3 = new RecordingInputPin();
-        RecordingInputPin p4 = new RecordingInputPin();
-        RecordingInputPin p5 = new RecordingInputPin();
-        RecordingInputPin p6 = new RecordingInputPin();
-        RecordingInputPin p7 = new RecordingInputPin();
-        port.connectBitTo(0, p0);
-        port.connectBitTo(1, p1);
-        port.connectBitTo(2, p2);
-        port.connectBitTo(3, p3);
-        port.connectBitTo(4, p4);
-        port.connectBitTo(5, p5);
-        port.connectBitTo(6, p6);
-        port.connectBitTo(7, p7);
+        RecordingInputPin[] p = new RecordingInputPin[8];
+        for (int i = 0; i < 8; i++) {
+            p[i] = new RecordingInputPin();
+            port.connectBitTo(i, p[i]);
+            p[i].reset();
+        }
 
         port.write(0xff, 0x7);
-        assertFalse(p7.writeCalled);
-        assertFalse(p6.writeCalled);
-        assertFalse(p5.writeCalled);
-        assertFalse(p4.writeCalled);
-        assertFalse(p3.writeCalled);
-        assertTrue(p2.writeCalled);
-        assertTrue(p1.writeCalled);
-        assertTrue(p0.writeCalled);
+        assertFalse(p[7].writeCalled);
+        assertFalse(p[6].writeCalled);
+        assertFalse(p[5].writeCalled);
+        assertFalse(p[4].writeCalled);
+        assertFalse(p[3].writeCalled);
+        assertTrue(p[2].writeCalled);
+        assertTrue(p[1].writeCalled);
+        assertTrue(p[0].writeCalled);
         assertEquals(0x7, port.read());
     }
 
