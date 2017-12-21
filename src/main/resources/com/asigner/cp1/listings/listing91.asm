bfe0e085360658679df16f17c6c02be9e375ed7e
Andreas Signer <asigner@gmail.com>
2017-12-21 23:49:31 +0100

    cleanup

--- src/main/resources/com/asigner/cp1/listings/listing91.asm
+++ src/main/resources/com/asigner/cp1/listings/listing91.asm
@@ -2,7 +2,7 @@
 
         .ORG 1
 
-loop    P1E 000     ;Port 1 einlesen und...
-        ANZ         ;anzeigen
-        P2A 000     ;an Port 2 ausgeben
-        SPU loop    ;vorn vorn beginnen
+loop    P1E 000     ; Port 1 einlesen und...
+        ANZ         ; anzeigen
+        P2A 000     ; an Port 2 ausgeben
+        SPU loop    ; vorn vorn beginnen
