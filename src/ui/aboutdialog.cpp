374265d377eb1217d4bcdee906feb77cd94b7ad5
Andreas Signer <asigner@gmail.com>
2024-07-12 13:34:55 +0200

    cleanup

--- src/ui/aboutdialog.cpp
+++ src/ui/aboutdialog.cpp
@@ -1,40 +1,13 @@
-/*
- * Copyright (c) 2023 Andreas Signer <asigner@gmail.com>
- *
- * This file is part of vicedebug.
- *
- * vicedebug is free software: you can redistribute it and/or
- * modify it under the terms of the GNU General Public License as
- * published by the Free Software Foundation, either version 3 of the
- * License, or (at your option) any later version.
- *
- * vicedebug is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with vicedebug.  If not, see <http://www.gnu.org/licenses/>.
- */
-
 #include "aboutdialog.h"
 
 #include <QVBoxLayout>
 #include <QPushButton>
 #include <QLabel>
-#include <QLineEdit>
-#include <QComboBox>
-#include <QGroupBox>
 
 #include "config.h"
 #include "resources.h"
 
 namespace kosmos_cp1::ui {
-
-namespace {
-constexpr const int kLogoW = 300;
-constexpr const int kLogoH = 131;
-}
 
 AboutDialog::AboutDialog(QWidget* parent) : QDialog(parent) {
     setupUI();
