17436bcab76bd546ab45ff33295ea6be69ace7a2
Andreas Signer <asigner@gmail.com>
2024-07-14 00:10:44 +0200

    add file header

--- src/ui/cpu/i8049disassemblywidget.cpp
+++ src/ui/cpu/i8049disassemblywidget.cpp
@@ -1,3 +1,22 @@
+/*
+ * Copyright (c) 2024 Andreas Signer <asigner@gmail.com>
+ *
+ * This file is part of kosmos-cp1.
+ *
+ * kosmos-cp1 is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * kosmos-cp1 is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with kosmos-cp1.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include "ui/cpu/i8049disassemblywidget.h"
 
 #include <QVBoxLayout>
