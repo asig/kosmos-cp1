7d0da66d13a3964b4253292193468e1d26f62546
Andreas Signer <asigner@gmail.com>
2024-10-09 00:19:18 +0200

    add build script for Windows

---
+++ build_win_release.bat
@@ -0,0 +1,29 @@
+set QT_DIR=C:\Qt
+set QT_HOME=%QT_DIR%\6.7.3
+set QT_TOOLS=%QT_DIR%\Tools
+set NSIS_HOME=%ProgramFiles(x86)%\NSIS
+
+set VERSION=0.1
+
+set PATH=%PATH%;%QT_TOOLS%\ninja
+set PATH=%PATH%;%QT_TOOLS%\CMake_64\bin
+set PATH=%PATH%;%QT_HOME%\mingw_64\bin
+set PATH=%PATH%;%NSIS_HOME%\Bin
+
+cmake.exe ^
+  -DQt6_DIR=%QT_HOME%\mingw_64\lib\cmake\Qt6 ^
+  -DQT_DIR=%QT_HOME%\mingw_64\lib\cmake\Qt6 ^
+  -DCMAKE_BUILD_TYPE=Release ^
+  -G "Ninja" ^
+  -B build\win\Release\ ^
+  .
+
+cd build\win\Release
+ninja
+cd ..\..\..
+
+windeployqt.exe ^
+  --release ^
+  build\win\Release\kosmos-cp1.exe
+
+makensis -DVERSION=%VERSION% -DARCH=x86_64 installer\win\KosmosCP1.nsi
