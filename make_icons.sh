fac5507692bf69eb33a2669f5b8835fd39d3c64b
Andreas Signer <asigner@gmail.com>
2024-10-27 22:45:00 +0100

    fix icon for Windows, add icons for Mac, add script used to create icons

---
+++ make_icons.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+TMPDIR=$(mktemp -d)
+
+# Create ICO file for Windows
+ICONS=""
+for sz in 16 24 32 48 256; do
+    f=${TMPDIR}/icon-${sz}x${sz}.png
+    ICONS="${ICONS} ${f}"
+    inkscape -w ${sz} artwork/icon.svg -o ${TMPDIR}/t.png
+    convert ${TMPDIR}/t.png -gravity center -background none -extent ${sz}x${sz} ${f}
+    # convert artwork/icon.svg -size ${sz}x${sz} ${f}
+done
+convert ${ICONS} resources/ui/icon.ico
+
+# Create icns file for Mac (    )
+mkdir ${TMPDIR}/icon.iconset
+for sz in 16 32 128 256 512; do
+    f=${TMPDIR}/icon.iconset/icon_${sz}x${sz}.png
+    inkscape -w ${sz} artwork/icon.svg -o ${TMPDIR}/t.png
+    convert ${TMPDIR}/t.png -gravity center -background none -extent ${sz}x${sz} ${f}
+
+    f=${TMPDIR}/icon-${sz}x${sz}@2x.png
+    (( sz2 = 2 * sz ))
+    inkscape -w ${sz2} artwork/icon.svg -o ${TMPDIR}/t.png
+    convert ${TMPDIR}/t.png -gravity center -background none -extent ${sz2}x${sz2} ${f}
+done
+png2icns resources/ui/icon.icns ${TMPDIR}/icon.iconset/*.png
