b47b2b98940d80a3e150dd5889d52efbc27a6694
Andreas Signer <asigner@gmail.com>
2024-11-07 22:53:26 +0100

    remove legacy java code

--- src/main/resources/com/asigner/cp1/listings/listing91.asm
+++
@@ -1,8 +0,0 @@
-; CP5-Listing 2: Schalter-Test
-
-        .ORG 1
-
-loop    P1E 000     ; Port 1 einlesen und...
-        ANZ         ; anzeigen
-        P2A 000     ; an Port 2 ausgeben
-        SPU loop    ; vorn vorn beginnen
